956236e4d3578209f578d9b26cdb0538ac3648d1
John1339 <104441019+John1339@users.noreply.github.com>
2022-05-24 18:08:23 -0500

    Update MedidaInternacional.java

--- TvDemo/src/PMedidas/MedidaInternacional.java
+++ TvDemo/src/PMedidas/MedidaInternacional.java
@@ -10,10 +10,20 @@ public class MedidaInternacional extends Medida {
   //
   // Fields
   //
+  /**
+  *Rodriguez Garcia Juan Alonso
+  *Creacion de atrivuto MEDIDA con valor de 2.45
+  */
     public final static double MEDIDAINT = 2.45;
   
   //
   // Constructors
+  //
+  /**
+  *Rodriguez Garcia Juan Alonso
+  *Implementacion de constructor MedidaInternacional
+  *
+  */
     public MedidaInternacional () { };
   //Herrera improvement public MedidaInternacional () { };
   
