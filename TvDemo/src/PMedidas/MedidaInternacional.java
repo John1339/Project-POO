830da51363b0895c33e7c3df0a75180b94f974e2
John1339 <104441019+John1339@users.noreply.github.com>
2022-05-26 13:41:13 -0500

    Update MedidaInternacional.java

--- TvDemo/src/PMedidas/MedidaInternacional.java
+++ TvDemo/src/PMedidas/MedidaInternacional.java
@@ -5,11 +5,21 @@ package PMedidas;
 /**
  * Class MedidaInternacional
  */
+/**
+*Rodriguez Garcia Juan Alonso
+*Inicializacion del constructor para la clase MedidaInternacional
+*/
 public class MedidaInternacional extends Medida {
   // Guadalupe Jaqueline Cruz Espinoza
   //
   // Fields
   //
+  /**
+  *Rodriguez Garcia Juan Alonso
+  *Se asigno valor de 2.45
+  *Se establece el valor para no poder modificarse
+  *No requiere de instanciacion para cceder 
+  */
     public final static double MEDIDAINT = 2.45;
   //
   // Constructor
